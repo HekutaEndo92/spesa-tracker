efcae3dad426843b49f69186f4d0292d6f551619
HekutaEndo92 <ettore-p@hotmail.it>
2026-09-09 19:35:40 +0200

    Refactor service worker for caching and fetching

--- sw.js
+++ sw.js
@@ -1,54 +1,33 @@
-// sw.js - Rende l'app funzionante offline
-const CACHE_NAME = 'spesa-tracker-v1';
-
-// File da mettere in cache
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  'https://unpkg.com/react@18/umd/react.development.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://unpkg.com/recharts@2.8.0/umd/Recharts.js'
+const CACHE = 'spesa-v2';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-// Installazione: metti tutto in cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Cache aperta');
-      return cache.addAll(ASSETS);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => {
+      return Promise.allSettled(FILES.map(f => c.add(f)));
     })
   );
   self.skipWaiting();
 });
 
-// Attivazione: rimuovi vecchie cache
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener('activate', e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// Fetch: prima cache, poi rete
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).catch(() => {
-        // Se offline e non in cache, mostra pagina offline
-        if (event.request.destination === 'document') {
-          return caches.match('/index.html');
-        }
-      });
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('./index.html'));
     })
   );
 });
